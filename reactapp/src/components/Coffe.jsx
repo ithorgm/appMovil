665976005cb92c09f2f0e100c6a7f0108bfecd14
ithorgm <99379459+ithorgm@users.noreply.github.com>
2024-09-19 22:30:26 -0600

    primer componente

---
+++ reactapp/src/components/Coffe.jsx
@@ -0,0 +1,9 @@
+export function Coffe() {
+    return (
+        <div style={{ backgroundColor: 'blue', height: '100vh' }}>
+            <h1>hola mundo</h1>
+        </div>
+    );
+}
+
+export default Coffe;
