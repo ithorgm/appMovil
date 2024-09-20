4917d43b3689258d6c79bf781013cc3a5b6f6cab
ithorgm <99379459+ithorgm@users.noreply.github.com>
2024-09-19 23:50:13 -0600

    vercel

--- reactapp/src/components/Usuarios.jsx
+++
@@ -1,6 +0,0 @@
-export function Usuarios(props){
-    return( <div>
-        <h1>{props.nombre}</h1>
-        <h1>{props.edad}</h1>
-        </div>)
-}
--- reactapp/src/index.jsx
+++ reactapp/src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Tareas from "./components/Tareas";
-import { Usuarios } from "./components/Usuarios";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
